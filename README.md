2a57b4a03f7798e72f20fb03ee5ddb5ee06a5ef3
alffox <alfonso.crisci.it@gmail.com>
2018-05-10 12:15:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Jasmine Workshop
+
+## Scope of the repo
+This project aims to provide a starter code to build Jasmine unit tests for this app: https://alffox.github.io/cat-clicker/
+
+## How to consume the code of this repo
+
+- **Method 1** (recommended - required for trainees):
+Download the repo on your PC locally and open jasmine-workshop/jasmine/jasmine-standalone-3.1.0/index.html with a browser of your choice.
+
+- **Method 2** (demo mode):
+Simply navigate to:
+
+## How to use the App
+
+The **Cat Clicker** app works simply like this: once you open the main page, click on any cat name in the left-handed side list to know more related info. Upon click, the cat's info and picture will be displayed on the right side of screen.
+
+After the first click, the **Admin button** will also be activated. Upon click, this button will show the Admin area. Such area shows the currently clicked cat data and allows updating name, URL (full URL path e.g. http://... is supported) and clicks through the "Save and Refresh" button. The operation can be cancelled with the "Cancel" button.
+
+## How to add Jasmine unit tests to the app
+
+Open the file jasmine-workshop/jasmine/jasmine-standalone-3.1.0/AppHelper.js and start adding your desired specs within the suites. Hints have been provided, e.g.:
+
+```it('$spec_description_here', function() {```
+
+This file will parse the logic of jasmine-workshop/jasmine/jasmine-standalone-3.1.0/src/app.js and will run tests against it.
+
+Once done, simply open jasmine-workshop/jasmine/jasmine-standalone-3.1.0/index.html in the broswer and check the test results at the bottom of the page. In case of failure for a test, click on it to get more related information
+
+## External libraries used
+jQuery - v.
+Jasmine - v. 3.1.0
+Jasmine-Jquery - v. 2.1.1
---
+++ jasmine/jasmine-standalone-3.1.0/src/app.js
@@ -0,0 +1,206 @@
+var model = {
+    //TODO: It could be nice to turn this model into an ajax call that fetches data from given site/API
+
+    cats: [{
+            clickCounter: 0,
+            name: 'Ashes',
+            imageURL: 'images/cat1_960x640.jpg',
+            authorName: 'Mikhail Vasilyev',
+            authorHyperlink: '@miklevasilyev',
+            siteName: 'Unsplash',
+            siteHyperlink: 'https://unsplash.com/'
+        },
+        {
+            clickCounter: 0,
+            name: 'Tiger',
+            imageURL: 'images/cat2_960x640.jpg',
+            authorName: 'Paul',
+            authorHyperlink: '@paul_',
+            siteName: 'Unsplash',
+            siteHyperlink: 'https://unsplash.com/'
+        },
+        {
+            clickCounter: 0,
+            name: 'Lion',
+            imageURL: 'images/cat3_960x640.jpg',
+            authorName: 'Kari Shea',
+            authorHyperlink: '@karishea',
+            siteName: 'Unsplash',
+            siteHyperlink: 'https://unsplash.com/'
+        },
+        {
+            clickCounter: 0,
+            name: 'Felix',
+            imageURL: 'images/cat4_960x640.jpg',
+            authorName: 'Pacto Visual',
+            authorHyperlink: '@pactovisual',
+            siteName: 'Unsplash',
+            siteHyperlink: 'https://unsplash.com/'
+        },
+        {
+            clickCounter: 0,
+            name: 'Napoleon',
+            imageURL: 'images/cat5_960x640.jpg',
+            authorName: 'Paul',
+            authorHyperlink: '@paul_',
+            siteName: 'Unsplash',
+            siteHyperlink: 'https://unsplash.com/'
+        }
+    ]
+};
+
+
+var octopus = {
+
+    init: function() {
+        view.init();
+
+        // Inializes empty clickedCat variable that will allow using it as empty argument to be filled later upon click on list
+        var clickedCat = null;
+
+        //Called now to always be ready to listen to clicks
+        octopus.getClickedCatData(clickedCat);
+
+    },
+
+    getCatNames: function() {
+
+        //Loops on cats object and returns only cat names array
+        return catNames = model.cats.map(a => a.name);
+    },
+
+    getClickedCatData: function() {
+        $(elemUl).off('click').click(function(e) {
+
+            //Assigns index position of clicked cat
+            var clickedCatIndexPos = $(e.target).index();
+
+            //Casts cat index position with model array position
+            clickedCat = model.cats[clickedCatIndexPos];
+
+            //Click counter has been updated, now let's render the latest cat data !
+            view.renderClickedCatInfo(clickedCat);
+        });
+    },
+
+    updateClickCounter: function(clickCounter) {
+        $(elemImg).click(function() {
+            clickedCat.clickCounter++;
+            view.updateClicks(clickedCat.clickCounter);
+        });
+    },
+
+    setClickedCatData: function(clickedCat) {
+        // This is unbunding/binding is necessary to remove previously clicked cat object references
+        $(elemSaveButton).off('click').click(function() {
+
+            view.hideShowform();
+
+            clickedCat.name = $(elemInputName).val();
+            clickedCat.imageURL = $(elemInputimageURL).val();
+            clickedCat.clickCounter = $(elemInputclickCounter).val();
+
+            view.renderList();
+            view.renderClickedCatInfo(clickedCat);
+
+            $(elemAdminButton).prop("disabled", true);
+
+        });
+    }
+
+};
+
+
+var view = {
+
+    init: function() {
+
+        // Store pointers to DOM elements for easy access later
+        elemCatList = $('.cat-list');
+        elemCatDashboard = $('.cat-dashboard');
+        elemUl = $('ul');
+
+        elemAdminButton = $('.admin-button');
+        elemSaveButton = $('.save-button');
+        elemCancelButton = $('.cancel-button');
+
+        elemHideable = $('.hideable');
+
+        elemInputName = $("input[name='name']");
+        elemInputimageURL = $("input[name='imageURL']");
+        elemInputclickCounter = $("input[name='clickCounter']");
+
+        view.renderList();
+
+        //If there is no cat in the list, it should not be possible to update it
+        elemAdminButton.prop("disabled", true);
+    },
+
+    //To parse elements that will only appear after the full cat data have been rendered
+    parseNewDOMElements: function() {
+        elemImg = $('img');
+        elemClickCounter = $('.click-counter');
+    },
+
+    renderList: function() {
+
+        $(elemUl).empty();
+
+        // On first page load, we want to load only cat names and not full data from the model, this is for scalability and performance reasons. There are high chances that some cats will never be clicked, especially if the list is long
+        octopus.getCatNames();
+
+        //Make a list from cat names
+        for (name = 0; name < catNames.length; name++) {
+            $(elemCatList).append('<li>' + catNames[name] + '</li>');;
+        }
+    },
+
+    renderClickedCatInfo: function(clickedCat) {
+
+        //Append all info related to clicked cat to DOM
+        $(elemCatDashboard).empty().append('<div class="cat-info"><div class="click-counter">Clicks: ' + clickedCat.clickCounter + '</div><img class="cat-image" src="' + clickedCat.imageURL + '" alt="A cat"><div class="cat-attribution">Photo by <a href="' + clickedCat.siteHyperlink + clickedCat.authorHyperlink + '">' + clickedCat.authorName + '</a> via <a href="' + clickedCat.siteHyperlink + '">' + clickedCat.siteName + '</a></div></div></div>');
+
+        //Enable Admin button
+        $(elemAdminButton).prop("disabled", false);
+
+        view.parseNewDOMElements();
+
+        //We want each click to increment click counter in the model
+        octopus.updateClickCounter(clickedCat);
+
+        view.fillCatForm(clickedCat);
+        octopus.getClickedCatData(clickedCat);
+        octopus.setClickedCatData(clickedCat);
+    },
+
+    updateClicks: function(clickCounter) {
+        $(elemClickCounter).text('Clicks: ' + clickedCat.clickCounter);
+    },
+
+    fillCatForm: function(clickedCat) {
+        elemAdminButton.off('click').click(function() {
+
+            view.hideShowform();
+
+            //Fill form with values from currently selected cat
+            $(elemInputName).val(clickedCat.name);
+            $(elemInputimageURL).val(clickedCat.imageURL);
+            $(elemInputclickCounter).val(clickedCat.clickCounter);
+
+            view.cancelAdmin();
+        });
+    },
+
+    cancelAdmin: function() {
+        elemCancelButton.off('click').click(function() {
+            view.hideShowform();
+        });
+    },
+
+    hideShowform: function() {
+        $(elemHideable).toggleClass('hidden');
+    }
+
+};
+// Ignite the app !
+octopus.init();
