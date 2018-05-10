5264f2ca6e97cd6b3123a1899b70b1a70ed6f818
alffox <alfonso.crisci.it@gmail.com>
2018-05-10 12:39:38 +0200

    style: Reindent HTML + JS

--- jasmine/jasmine-standalone-3.1.0/spec/AppHelper.js
+++ jasmine/jasmine-standalone-3.1.0/spec/AppHelper.js
@@ -8,41 +8,32 @@
  */
 $(function() {
     describe('Cats', function() {
-        it('ensures cats are defined', function() {
-        });
+        it('ensures cats are defined', function() {});
 
-        it('ensures cat names are defined and not empty', function() {
-        });
+        it('ensures cat names are defined and not empty', function() {});
 
-        it('ensures the model list lenght is matching with the rendered list lenght', function() {
-        });
+        it('ensures the model list lenght is matching with the rendered list lenght', function() {});
 
     });
 
     describe('Admin Area', function() {
 
-        it('ensures Admin form is hidden by default', function() {
-        });
+        it('ensures Admin form is hidden by default', function() {});
 
-        it('ensures Admin button becomes enabled after clicking on the first cat', function() {
-        });
+        it('ensures Admin button becomes enabled after clicking on the first cat', function() {});
 
-        it('ensures Admin form is shown after clicking on the Admin button', function() {
-        });
+        it('ensures Admin form is shown after clicking on the Admin button', function() {});
 
-      });
+    });
 
     describe('Dashboard Area after first click on the cat list', function() {
 
-        it('shows a cat photo', function() {
-        });
+        it('shows a cat photo', function() {});
 
-        it('shows photo attribution', function() {
-        });
+        it('shows photo attribution', function() {});
 
-        it('has a click counter that increments on each click', function() {
-        });
+        it('has a click counter that increments on each click', function() {});
 
-      });
+    });
 
 }());
